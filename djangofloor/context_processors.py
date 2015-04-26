3c5f6dc1fb7f34b3142347218f43ab8a52d52f5e
mgallet <mgallet@spibol.19pouces.net>
2015-04-26 20:50:34 +0200

    minor bug fix

--- djangofloor/context_processors.py
+++ djangofloor/context_processors.py
@@ -8,7 +8,7 @@ __author__ = 'flanker'
 
 def context_base(request):
     return {
-        'df_remote_authenticated': request.df_remote_authenticated,
+        'df_remote_authenticated': getattr(request, 'df_remote_authenticated', False),
         'df_project_name': settings.FLOOR_PROJECT_NAME,
         'df_user': request.user,
         'df_language_code': settings.LANGUAGE_CODE,
