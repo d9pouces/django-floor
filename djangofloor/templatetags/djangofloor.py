55b832d308c2eeec87810f1215f622a563061a99
mgallet <mgallet@spibol.19pouces.net>
2015-05-02 09:46:35 +0200

    Changement sur les templatetags (élimination d'un doublon), nouvelle page d'accueil

--- djangofloor/templatetags/djangofloor.py
+++ djangofloor/templatetags/djangofloor.py
@@ -8,16 +8,23 @@ register = template.Library()
 
 
 @register.simple_tag
-def fontawesome_icon(icon, large=False, fixed=False, spin=False, li=False, rotate=False):
-    return '<i class="fa fa-{icon} {large} {fixed} {spin} {li} {rotate}"></i>'.format(
-        icon=icon,
-        large='fa-lg' if large is True else '',
-        fixed='fa-fw' if fixed else '',
-        spin='fa-spin' if spin else '',
-        li='fa-li' if li else '',
-        rotate='fa-rotate-%s' % str(rotate) if rotate else ''
+def fontawesome_icon(name, large=False, fixed=False, spin=False, li=False, rotate=None, border=False, color=None):
+    if isinstance(large, int) and 2 <= large <= 5:
+        large = ' fa-%dx' % large
+    elif large:
+        large = ' fa-lg'
+    else:
+        large = ''
+    return '<i class="fa fa-{name}{large}{fixed}{spin}{li}{rotate}{border}"{color}></i>'.format(
+        name=name,
+        large=large,
+        fixed=' fa-fw' if fixed else '',
+        spin=' fa-spin' if spin else '',
+        li=' fa-li' if li else '',
+        rotate=' fa-rotate-%s' % str(rotate) if rotate else '',
+        border=' fa-border' if border else '',
+        color='style="color:%s;"' % color if color else ''
     )
-
 
 if __name__ == '__main__':
     import doctest
