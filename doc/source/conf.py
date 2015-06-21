6a0a76373005e559623625db6916bcced63ede6c
mgallet <mgallet@spibol.19pouces.net>
2015-06-21 12:06:41 +0200

    new doc theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -100,12 +100,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'bgcolor': '#FCFCFC; background: #FCFCFC url("fond_article.png") no-repeat center 10em; ', }
+# html_theme_options = {'bgcolor': '#FCFCFC; background: #FCFCFC url("fond_article.png") no-repeat center 10em; ', }
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
