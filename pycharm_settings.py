9fef28821e48e6d21cd3c8cff742b3a354b22cd5
mgallet <mgallet@spibol.19pouces.net>
2015-11-11 14:59:59 +0100

    nouvelles mises à jour pour les versions plus récentes de Django && DjangoFloor

--- pycharm_settings.py
+++ pycharm_settings.py
@@ -32,7 +32,7 @@ DATABASE_PORT = ''
 DATABASE_USER = ''
 FLOOR_BACKUP_SINGLE_TRANSACTION = False
 DATA_PATH = './django_data/data'
-DEBUG = False
+DEBUG = True
 DEBUG_TOOLBAR_CONFIG = {'JQUERY_URL': None}
 DEBUG_TOOLBAR_PANELS = ['debug_toolbar.panels.versions.VersionsPanel', 'debug_toolbar.panels.timer.TimerPanel', 'debug_toolbar.panels.profiling.ProfilingPanel',
                         'debug_toolbar.panels.settings.SettingsPanel', 'debug_toolbar.panels.headers.HeadersPanel', 'debug_toolbar.panels.request.RequestPanel', 'debug_toolbar.panels.sql.SQLPanel',
