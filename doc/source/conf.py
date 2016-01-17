6b7efb5d528c9a07d1881b6dd68d6e0380524a52
mgallet <mgallet@spibol.19pouces.net>
2016-01-17 15:47:43 +0100

    detect identical documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'DjangoFloor'
 # noinspection PyShadowingBuiltins
-copyright = '2015, Matthieu Gallet'
+copyright = '2016, Matthieu Gallet'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -234,7 +234,7 @@ man_pages = [
 epub_title = 'DjangoFloor'
 epub_author = 'Matthieu Gallet'
 epub_publisher = '19pouces.net'
-epub_copyright = '2015, Matthieu Gallet'
+epub_copyright = '2016, Matthieu Gallet'
 
 # The language of the text. It defaults to the language option
 # or en if the language is not set.
