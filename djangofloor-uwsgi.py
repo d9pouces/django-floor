abf817accc7d22c1d15036cb7cb2947a43c44a16
mgallet <flanker@19pouces.net>
2016-11-13 18:07:45 +0100

    start of the migration to replace djangofloor

--- djangofloor-uwsgi.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-from __future__ import unicode_literals, absolute_import
-from djangofloor.scripts import uwsgi
-__author__ = 'Matthieu Gallet'
-
-uwsgi()
--- djangofloor/empty.py
+++
@@ -1,3 +0,0 @@
-# coding=utf-8
-""" Empty module used when no settings file is given.
-"""
