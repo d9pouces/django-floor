5de77a2f15b5dcd2339129f6e1e02721d1e09442
d9pouces <github@19pouces.net>
2017-07-23 11:42:22 +0200

    small doc update

--- djangofloor/templates/djangofloor/vagrant/run_package.sh
+++ djangofloor/templates/djangofloor/vagrant/run_package.sh
@@ -4,14 +4,14 @@ if [ -z "${IS_INSTALLED}" ]; then
     sudo rm -rf "{{ install_dir.1 }}"
 fi
 sudo dpkg -i "{{ tmp_dir.1 }}/{{ package_filename }}"
-sudo -u "{{ DF_MODULE_NAME }}" "{{ processes.django.command_line }}" migrate
+sudo -H -u "{{ DF_MODULE_NAME }}" "{{ processes.django.command_line }}" migrate
 cat << EOF | sudo tee "{{ install_dir.1 }}/etc/{{ DF_MODULE_NAME }}/settings.ini"
 [global]
 listen_address = 0.0.0.0:{{ SERVER_PORT }}
 server_url = http://localhost:10080/
 EOF
 sudo apt-get install -y redis-server
-cat << EOF | sudo -u "{{ DF_MODULE_NAME }}" "{{ processes.django.command_line }}" shell
+cat << EOF | sudo -H -u "{{ DF_MODULE_NAME }}" "{{ processes.django.command_line }}" shell
 from django.contrib.auth.models import User
 if User.objects.filter(username='admin').count() == 0:
     u = User(username='admin')
@@ -24,14 +24,14 @@ if User.objects.filter(username='user').count() == 0:
     u.set_password('user')
     u.save()
 EOF
-{% if processes.celery %}{% for queue in expected_celery_queues %}sudo -u "{{ DF_MODULE_NAME }}" "{{ processes.celery.command_line }}" worker -Q "{{ queue }}"  -n '%h-{{ queue }}' &
+{% if processes.celery %}{% for queue in expected_celery_queues %}sudo -H -u "{{ DF_MODULE_NAME }}" "{{ processes.celery.command_line }}" worker -Q "{{ queue }}"  -n '%h-{{ queue }}' &
 {% endfor %}
 {% endif %}
 echo "a standard user named “user” has been created (with password “user”)"
 echo "an admin user named “admin” has been created (with password “admin”)"
 echo "You can stop the web process with <Ctrl>-<C>"
-{% if processes.aiohttp %}sudo -u "{{ DF_MODULE_NAME }}" "{{ processes.aiohttp.command_line }}"
-{% elif processes.gunicorn %}sudo -u "{{ DF_MODULE_NAME }}" "{{ processes.gunicorn.command_line }}"
+{% if processes.aiohttp %}sudo -H -u "{{ DF_MODULE_NAME }}" "{{ processes.aiohttp.command_line }}"
+{% elif processes.gunicorn %}sudo -H -u "{{ DF_MODULE_NAME }}" "{{ processes.gunicorn.command_line }}"
 {% endif %}
 {% if expected_celery_queues %}echo "To stop all Celery workers, you can use the following command"
 echo 'sudo kill `ps aux   | grep "^{{ DF_MODULE_NAME }}"  | awk "{print \$2}"`'
