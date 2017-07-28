eb75586d749a1b25d36863bdd094472425cd7133
d9pouces <github@19pouces.net>
2017-07-28 07:43:39 +0200

    remove Py2 support

--- djangofloor/forms.py
+++ djangofloor/forms.py
@@ -1,4 +1,3 @@
-# -*- coding=utf-8 -*-
 """Forms used in default views
 ===========================
 
--- djangofloor/signals/html.py
+++ djangofloor/signals/html.py
@@ -1,4 +1,3 @@
-# -*- coding=utf-8 -*-
 """Python shortcuts for JS signals
 ===============================
 
