26aaebec3bfca530c8bd7fddd08e88afb07b1ad5
d9pouces <github@19pouces.net>
2018-01-07 22:58:34 +0100

    allow to django-pipeline to work

--- EasyDemo/easydemo/defaults.py
+++ EasyDemo/easydemo/defaults.py
@@ -1,9 +1,6 @@
 DF_INDEX_VIEW = 'easydemo.views.IndexView'
-DEVELOPMENT = True
-DEBUG = False
+DEVELOPMENT = False
 PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.jsmin.JSMinCompressor'
 PIPELINE_CSS_COMPRESSOR = 'djangofloor.templatetags.pipeline.RcssCompressor'
 PIPELINE_COMPILERS = ['djangofloor.templatetags.pipeline.PyScssCompiler']
 DF_CSS = ['scss/easydemo.scss']
-# PIPELINE_ENABLED = True
-# USE_PIPELINE = True
