a98b078fc480b32868d258d0644777522162d4ca
Matthieu Gallet <github@19pouces.net>
2019-02-17 17:19:39 +0100

    bug fix in config values - blackifying

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
+import os
+
 from djangofloor.scripts import django, set_env
 
 __author__ = "Matthieu Gallet"
+os.environ["DJANGO_SETTINGS_MODULE"] = "djangofloor.conf.settings"
 
 set_env(command_name="djangofloor-django")
 django()
