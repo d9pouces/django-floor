6880921bacb5964ad1e52cb8c949f6471f379553
Matthieu Gallet <github@19pouces.net>
2020-01-02 23:14:33 +0100

    classifiers fix

--- setup.py
+++ setup.py
@@ -109,7 +109,6 @@ setup(
         "Framework :: Django :: 2.1",
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.0",
-        "Framework :: Django :: 3.1",
         "Natural Language :: English",
         "Natural Language :: French",
         "Operating System :: MacOS :: MacOS X",
